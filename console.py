842b7df2a67d45a53a4c8f23868ea928b931d3f4
EliasMartincorre <martincorrea792@gmail.com>
2023-07-06 19:18:30 -0300

    console

--- console.py
+++ console.py
@@ -1,29 +1,123 @@
 #!/usr/bin/python3
+"""
+Console for the project
+"""
 import cmd
+import sys
+import os
+import shlex
+from models.base_model import BaseModel
+from models import storage
+from models.engine.file_storage import FileStorage
+
 
 
 class HBNBCommand(cmd.Cmd):
-    """Simple command processor example."""
+    """Command line for work in front and backend"""
+
+    prompt = '(hbnb) '
+    file = None
+    classes = {'BaseModel': BaseModel}
 
     def emptyline(self):
-        """Ignores empty prompts"""
+        """nothing when empty argument and enter"""
         pass
 
+    def do_quit(self, arg):
+        """ Quit command to exit the program"""
+        return True
+
     def do_EOF(self, line):
-        """exit the program"""
+        """ End of file ctrl +d"""
         return True
 
-    def do_quit(self, line):
-        """exit the program"""
-        return True
+    def do_create(self, args):
+        """Creates a new instance of BaseModel, saves it
+        (to the JSON file) and prints the id."""
+        if len(args) < 1:
+            print("** class name missing **")
+            return
+        try:
+            args = shlex.split(args)
+            ninstance = eval(args[0])()
+            ninstance.save()
+            print(ninstance.id)
+        except Exception:
+            print("** class doesn't exist **")
 
-    prompt = "(hbnb) "
+    def do_show(self, argv):
+        """Prints the string representation of an
+        instance based on the class name and id"""
+        enter = argv.split()
+        if not enter:
+            print("** class name missing **")
+        elif enter[0] not in HBNBCommand.classes.keys():
+            print("** class doesn't exist **")
+        elif len(enter) < 2:
+            print("** instance id missing **")
+        else:
+            key = enter[0] + '.' + enter[1]
+            storage.reload()
+            if key not in storage.all():
+                print("** no instance found **")
+            else:
+                objects = storage.all()
+                if key in objects.keys():
+                    print(objects[key])
 
-    def help_quit(self):
-        """help quit command"""
-        print("Quit command to exit the program")
-        print("\n")
+    def do_destroy(self, argv):
+        """Deletes an instance based on the class name and id"""
+        input = argv.split()
+        if not input:
+            print("** class name missing **")
+        elif input[0] not in HBNBCommand.classes.keys():
+            print("** class doesn't exist **")
+        elif len(input) < 2:
+            print("** instance id missing **")
+        else:
+            key = input[0] + '.' + input[1]
+            storage.reload()
+            if key not in storage.all():
+                print("** no instance found **")
+            else:
+                storage.all().pop(key)
+                storage.save()
+
+    def do_all(self, argv):
+        input = argv.split()
+        if not input:
+            print(list(storage.all()))
+        elif input[0] not in HBNBCommand.classes.keys():
+            print("** class doesn't exist **")
+        else:
+            list_all = []
+            for o_id in storage.all().keys():
+                if o_id.split(".")[0] == input[0]:
+                    list_all.append(str(storage.all()[o_id]))
+            print(list_all)
+
+    def do_update(self, argv):
+        enter = argv.split()
+        if not enter:
+            print("** class name missing **")
+        elif enter[0] not in HBNBCommand.classes.keys():
+            print("** class doesn't exist **")
+        elif len(enter) < 2:
+            print("** instance id missing **")
+        else:
+            key = enter[0] + '.' + enter[1]
+            storage.reload()
+            if key not in storage.all():
+                print("** no instance found **")
+            elif len(enter) < 3:
+                print("** attribute name missing **")
+            elif len(enter) < 4:
+                print("** value missing **")
+            else:
+                setattr(storage.all()[key], enter[2], enter[3])
+                storage.save()
 
 
 if __name__ == '__main__':
+    """infinity loops"""
     HBNBCommand().cmdloop()
