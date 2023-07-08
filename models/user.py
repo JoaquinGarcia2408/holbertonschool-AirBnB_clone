8ccfc9d24561a4758a9dfbfffc1040befcf63ef8
EliasMartincorre <martincorrea792@gmail.com>
2023-07-07 21:19:37 -0300

    user

--- models/user.py
+++ models/user.py
@@ -1,20 +1,11 @@
 #!/usr/bin/python3
-"""
-Keyword arguments:
-argument -- description
-Return: return_description
-"""
-
+"""This module defines a class User"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    "Inherits from BAseModel"""
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(self, *args, **kwargs)
+    """This class defines a user by various attributes"""
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
