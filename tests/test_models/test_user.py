73790abde3a2a9bfd7453ff1a7fda7ee627e3703
EliasMartincorre <martincorrea792@gmail.com>
2023-07-08 13:30:56 -0300

    tests_user

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -3,7 +3,6 @@
 todos los test para la clase
 user
 """
-
 import unittest
 from models.user import User
 from models.base_model import BaseModel
@@ -56,3 +55,32 @@ class TestUser(unittest.TestCase):
         self.assertIn("last_name", usuario.__dict__)
         self.assertIn("id", usuario.to_dict())
         self.assertIn("id", usuario.__dict__)
+
+    def test_attr_type(self):
+        a = User()
+        self.assertEqual(type(a.first_name), str)
+        self.assertEqual(type(a.last_name), str)
+        self.assertEqual(type(a.email), str)
+        self.assertEqual(type(a.password), str)
+
+    def test_public_attr(self):
+        a = User()
+        a.first_name = "martin"
+        a.last_name = "Correa"
+        a.email = "martin@elias.com"
+        a.password = "hola"
+        self.assertEqual(a.first_name, "martin")
+        self.assertEqual(a.last_name, "Correa")
+        self.assertEqual(a.email, "martin@elias.com")
+        self.assertEqual(a.password, "hola")
+
+    def test_attr(self):
+        a = User()
+        self.assertNotIn("first_name", a.__dict__)
+        self.assertNotIn("last_name", a.__dict__)
+        self.assertNotIn("email", a.__dict__)
+        self.assertNotIn("password", a.__dict__)
+
+    def test_instance(self):
+        a = User()
+        self.assertIsInstance(a, type(User()))
